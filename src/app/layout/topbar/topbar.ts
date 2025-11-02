4cb891ad12f26b859be3fc21b067604121acfbef
Zeyad_nabil <zeyad.nabil.2005@gmail.com>
2025-11-03 00:45:21 +0200

    Adding bootstrap and also adding startup folders (authentication,home,layout)

---
+++ src/app/layout/topbar/topbar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topbar',
+  imports: [],
+  templateUrl: './topbar.html',
+  styleUrl: './topbar.css',
+})
+export class Topbar {
+
+}
