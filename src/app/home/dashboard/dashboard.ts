7286ae455e63bb84aa26159b29795c4ed88544ba
Zeyad_nabil <zeyad.nabil.2005@gmail.com>
2025-11-17 23:38:16 +0200

    Adding the dashboard with 2 views one for admin view and one for doctor view

--- src/app/home/dashboard/dashboard.ts
+++ src/app/home/dashboard/dashboard.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
 export class Dashboard {
-
+  user = {
+    role: 'doctor'
+  };
 }
